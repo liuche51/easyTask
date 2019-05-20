6734e6f48e8441ef3b9041fa10fe42170085519e
liuche <cheliu@sfmail.sf-express.com>
2019-05-20 16:50:31 +0800

    编写核心时间分片环形队列

--- easyTask-core/src/main/java/liuche/opensource/easyTask/core/AnnularQueue.java
+++ easyTask-core/src/main/java/liuche/opensource/easyTask/core/AnnularQueue.java
@@ -1,7 +1,54 @@
 package liuche.opensource.easyTask.core;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class AnnularQueue {
-    public void run(){
+    private static Logger log = LoggerFactory.getLogger(AnnularQueue.class);
+    /**
+     * 任务调度线程池
+     */
+   static ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<Runnable>(5));
+    /**
+     * 工作任务线程池
+     */
+    static ThreadPoolExecutor workers = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<Runnable>(5));
+    static Slice[] slices=new Slice[60];
+    public static void start(){
+        int lastSecond=0;
+        while (true){
+            int second= LocalDateTime.now().getSecond();
+            if(second==lastSecond){
+                try {
+                    Thread.sleep(500l);
+                    continue;
+                }catch (Exception e){
+
+                }
+            }
+            final Slice slice=slices[second];
+            log.debug("已执行时间分片:{}",second);
+            executor.submit(new Runnable() {
+                public void run() {
+                    List<Task> tasks=slice.getList();
+                    tasks.forEach(x->{
+                        if(System.currentTimeMillis()>=x.getEndTimestamp()){
+                            workers.submit(x);
+                        }
+                    });
+
+                }
+            });
+        }
+
 
     }
 }
---
+++ easyTask-core/src/main/java/liuche/opensource/easyTask/core/Slice.java
@@ -0,0 +1,15 @@
+package liuche.opensource.easyTask.core;
+
+import java.util.List;
+
+public class Slice {
+    private List<Task> list;
+
+    public List<Task> getList() {
+        return list;
+    }
+
+    public void setList(List<Task> list) {
+        this.list = list;
+    }
+}
